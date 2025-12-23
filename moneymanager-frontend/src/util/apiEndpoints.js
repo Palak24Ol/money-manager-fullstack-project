8d6a1d54ea8d7cdedc77bbb4687d4571409c45ec
Palak Jaiswal <palakj.ug23.cs@nitp.ac.in>
2025-12-23 12:45:27 +0530

    link

--- moneymanager-frontend/src/util/apiEndpoints.js
+++ moneymanager-frontend/src/util/apiEndpoints.js
@@ -1,4 +1,4 @@
-export const BASE_URL = "http://localhost:8080/api/v1.0";
+export const BASE_URL = "https://money-manager-fullstack-project-production.up.railway.app/api/v1.0";
 const CLOUDINARY_CLOUD_NAME = "dhadf5h7j";
 
 export const API_ENDPOINTS = {
