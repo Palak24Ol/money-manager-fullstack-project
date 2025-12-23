e5f6a433b312bd22a11563ba747a1eb0cdd09caa
Palak Jaiswal <palakj.ug23.cs@nitp.ac.in>
2025-12-23 14:10:01 +0530

    commit

--- moneymanager-backend/src/main/java/com/palak/moneymanager/config/SecurityConfig.java
+++ moneymanager-backend/src/main/java/com/palak/moneymanager/config/SecurityConfig.java
@@ -33,8 +33,15 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         httpSecurity.cors(Customizer.withDefaults())
                 .csrf(AbstractHttpConfigurer::disable)
-                .authorizeHttpRequests(auth -> auth.requestMatchers("/status", "/health", "/register", "/activate", "/login").permitAll()
-                        .anyRequest().authenticated())
+                .authorizeHttpRequests(auth -> auth
+                   .requestMatchers(
+                      "/status",
+                      "/health",
+                      "/api/v1.0/auth/**"
+                    ).permitAll()
+                    .anyRequest().authenticated()
+           )
+
                         .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                         .addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
         return httpSecurity.build();
